c98cbab856fe7d153853b0c0ec3d279ee6ff09d7
linxiaolong <1394139539@qq.com>
2017-11-29 18:35:14 +0800

    变量: 类型放在后面

---
+++ tour/8.variables.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// var 语句定义了一个变量的列表；跟函数的参数列表一样， **类型在后面**
+var c, python, java bool
+
+func main()  {
+	var i int
+	// 0 false false false
+	fmt.Println(i, c, python, java)
+}
