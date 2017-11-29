50563df1436af4631668c9d3580ad6381a8edee6
linxiaolong <1394139539@qq.com>
2017-11-29 18:33:05 +0800

    命名返回值

---
+++ tour/7.named-results.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+// Go 的返回值可以被命名，并且就像在函数体开头声明的变量那样使用
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	// 返回值的命名在函数名上面声明了
+	// 没有参数的 return 语句返回各个返回变量的当前值
+	return
+}
+
+func main()  {
+	fmt.Println(split(17)) // 得到 "7 10"
+}
