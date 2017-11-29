107c3303d237b0e05d1376d732bc20fc6b30cb19
nounen <281580047@qq.com>
2017-11-29 20:12:17 +0800

    零值: 没有初始化时的赋值

---
+++ tour/12.zero.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main()  {
+	var i int
+	var f float64
+	var b bool
+	var s string
+
+	// 变量在定义时 **没有明确的初始化** 时会赋值为 **零值**
+	// 0 0 false ""
+	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+}
