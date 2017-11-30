a76f1ca2ffb412742ce187c49c7d03d0861c89d1
nounen <281580047@qq.com>
2017-11-30 13:53:33 +0800

    数组array

---
+++ tour/3.moretypes/6.array.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// 数组: 类型 [n]T 是一个有 n 个类型为 T 的值的数组
+
+// 数组的长度是其类型的一部分，因此数组不能改变大小。 这看起来是一个制约，但是请不要担心； Go 提供了更加便利的方式来使用数组。
+func main()  {
+	var a[2] string;
+	
+	a[0] = "Hello"
+	a[1] = "World"
+
+	// Hello World
+	fmt.Println(a[0], a[1])
+
+	// [Hello World]	
+	fmt.Println(a)
+}
