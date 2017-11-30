8812ef33371629ed90c07952c3493eb2a742e469
nounen <281580047@qq.com>
2017-11-30 11:24:33 +0800

    defer栈: 后进先出

---
+++ tour/2.flowcontrol/13.defer-multi.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println("counting")
+
+	// defer 栈: **后进先处** 的顺序调用被延迟的函数调用
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+
+	fmt.Println("done")	
+	/*
+	counting
+	done
+	9
+	8
+	7
+	6
+	5
+	4
+	3
+	2
+	1
+	0
+	*/
+}
