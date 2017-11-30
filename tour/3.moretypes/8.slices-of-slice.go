7780ebd56966c7b8cd30621da605063d64c39f37
nounen <281580047@qq.com>
2017-11-30 14:17:53 +0800

    slice 的 slice

---
+++ tour/3.moretypes/8.slices-of-slice.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// slice 的 slice: slice 可以包含任意的类型，包括另一个 slice
+func main()  {
+	game := [][]string{
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+	}
+
+	game[0][0] = "X"
+	game[2][2] = "O"
+	game[2][0] = "X"
+	game[1][0] = "O"
+	game[0][2] = "X"
+
+	printBoard(game)
+}
+
+func printBoard(s [][]string)  {
+	/*
+	X_X
+	O__
+	X_O
+	*/
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%s\n", strings.Join(s[i], ""))
+	}
+}
