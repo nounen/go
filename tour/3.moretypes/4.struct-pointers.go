5ed3bcabf80f71e955e76d623bfa619705449f7c
nounen <281580047@qq.com>
2017-11-30 13:45:52 +0800

    结构体字段可以通过结构体指针来访问

---
+++ tour/3.moretypes/4.struct-pointers.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+	X int
+	Y int
+}
+
+/*
+结构体指针
+	结构体字段可以通过结构体指针来访问
+	通过指针间接的访问是透明的
+*/
+func main() {
+	v := Vertex{1, 2}
+	p := &v
+	p.X = 1e9
+	// {1000000000 2}
+	fmt.Println(v)
+}
