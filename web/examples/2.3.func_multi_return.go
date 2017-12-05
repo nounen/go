46eaafe9514d83e681fe74cb1a4f7558a56611e2
nounen <1394139539@qq.com>
2017-12-05 22:29:05 +0800

    函数多个返回值的另一种写法: 把返回值声明在函数上

--- web/examples/2.3.func_multi_return.go
+++ web/examples/2.3.func_multi_return.go
@@ -5,9 +5,15 @@ import (
 )
 
 // 函数支持多个返回值
-
 func SumAndProduct(A, B int) (int, int) {
 	return A + B, A * B
+}
+
+// 另一种多个返回值的写法
+func SumAndProductAnothor(A, B int) (sum int, product int) {
+	sum = A + B
+	product = A * B
+	return  // 此处省略返回值, 因为在函数定义事已经声明了
 }
 
 func main() {
@@ -15,7 +21,10 @@ func main() {
 	y := 4
 
 	result1, result2 := SumAndProduct(x, y)
-
 	fmt.Println(result1)
 	fmt.Println(result2)
+
+	sum, product := SumAndProductAnothor(x, y)
+	fmt.Println(sum)
+	fmt.Println(product)
 }
