a8895f4176b72d49028a17f9933a0cf6a5f584e2
nounen <281580047@qq.com>
2017-12-05 09:27:15 +0800

    array example

---
+++ web/examples/2.2.array.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var arr [10]int; // 声明一个长度为10的int类型数组
+	arr[0] = 10;
+	arr[1] = 1;
+
+	fmt.Printf("arr is %v\n", arr)
+	fmt.Printf("first is %d\n", arr[0])
+	fmt.Printf("last is %v\n", arr[9])
+
+	a := [3]int{1, 2, 3}
+	fmt.Printf("%v \n", a)
+
+	c := [...]int{4, 5, 6}
+	fmt.Printf("%v\n", c)
+}
