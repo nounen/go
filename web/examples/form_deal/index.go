e464041a9135670c1fecda7a0ae81a2bbef88cd6
nounen <281580047@qq.com>
2017-12-12 16:51:40 +0800

    验证表单的输入

--- web/examples/form_deal/index.go
+++ web/examples/form_deal/index.go
@@ -6,12 +6,16 @@ import (
 	"log"
 	"net/http"
 	"strings"
-	"net/url"
+	"strconv"
 )
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	//解析url传递的参数，对于POST则解析响应包的主体（request body）
 	r.ParseForm()
+
+	if r.URL.Path == "/favicon.ico" {
+		return
+	}
 
 	fmt.Println("path", r.URL.Path)
 	//fmt.Println("scheme", r.URL.Scheme)
@@ -28,35 +32,76 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello lll!") //这个写入到w的是输出到客户端的
 
 	// request.Form是一个 url.Values 类型. TODO: 没搞懂 url.Values 是什么
-	v := url.Values{}
-	fmt.Println("v:", v)
+	//v := url.Values{}
+	//fmt.Println("v:", v)
+	//
+	//v.Set("name", "Ava")
+	//v.Add("friend", "Jess")
+	//v.Add("friend", "Sarah")
+	//v.Add("friend", "Zoe")
+	//// v.Encode() == "name=Ava&friend=Jess&friend=Sarah&friend=Zoe"
+	//fmt.Println(v.Get("name"))
+	//fmt.Println(v.Get("friend"))
+	//fmt.Println(v["friend"])
 
-	v.Set("name", "Ava")
-	v.Add("friend", "Jess")
-	v.Add("friend", "Sarah")
-	v.Add("friend", "Zoe")
-	// v.Encode() == "name=Ava&friend=Jess&friend=Sarah&friend=Zoe"
-	fmt.Println(v.Get("name"))
-	fmt.Println(v.Get("friend"))
-	fmt.Println(v["friend"])
-
-	fmt.Println("==================================")
+	fmt.Println("==================================\n")
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 
+	input := r.Form
+
 	// r.Form里面包含了所有请求的参数，比如URL中query-string、POST的数据、PUT的数据，所以当你在URL中的query-string字段和POST冲突时，会保存成一个slice，里面存储了多个值，Go官方文档中说在接下来的版本里面将会把POST、GET这些数据分离开来。
-	fmt.Println(r.Form)
+	fmt.Println("Form:", input)
 
 	fmt.Println("method:", r.Method)
 
 	if r.Method == "GET" {
 		t, _ := template.ParseFiles("login.html")
+
 		log.Println(t.Execute(w, nil))
 	} else {
-		fmt.Println("username:", r.Form["username"])
-		fmt.Println("password:", r.Form["password"])
+		// 校验密码
+		if len(input.Get("password"))  == 0 {
+			fmt.Println("请输出密码", input.Get("password"))
+		}
+
+		// 校验年龄
+		getAge, err := strconv.Atoi(input.Get("age"))
+
+		if err != nil {
+			fmt.Println("年龄请输入整数", getAge)
+		}
+
+		// 校验下拉框
+		fruits := []string{
+			"apple", "pear", "banana",
+		}
+
+		fruit := input.Get("fruit")
+
+		for _, item := range fruits {
+			if item == fruit {
+				fmt.Println("下拉选项正确")
+			}
+		}
+
+		// 校验单选框
+		genders := []string{
+			"1", "2",
+		}
+
+		for _, item := range genders {
+			if item == input.Get("gender") {
+				fmt.Println("单选框正确")
+			}
+		}
+
+		//fmt.Println("username:", r.Form["username"])
+		//fmt.Println("password:", r.Form["password"])
+
+		fmt.Println("==================================\n")
 	}
 }
 
