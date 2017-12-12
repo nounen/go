dfc947363a313ff7c4303606f07db053920a9759
nounen <281580047@qq.com>
2017-12-12 16:08:41 +0800

    模板后缀改成 .html

--- web/examples/form_deal/index.go
+++ web/examples/form_deal/index.go
@@ -52,7 +52,7 @@ func login(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("method:", r.Method)
 
 	if r.Method == "GET" {
-		t, _ := template.ParseFiles("login.gtpl")
+		t, _ := template.ParseFiles("login.html")
 		log.Println(t.Execute(w, nil))
 	} else {
 		fmt.Println("username:", r.Form["username"])
