b4c257adab088253725c545808a7722246888aad
nounen <281580047@qq.com>
2017-12-14 17:09:52 +0800

    常量与枚举

---
+++ the_way_to_go/04.3.const.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+// 枚举
+const (
+	Unknown = 0
+	Female  = 1
+	Male    = 2
+)
+
+func main() {
+	fmt.Println(Unknown)
+	fmt.Println(Female)
+	fmt.Println(Male)
+}
