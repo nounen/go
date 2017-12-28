7aebb03d52eb65196fc19d9888aacf9096b32a9e
nounen <281580047@qq.com>
2017-12-28 11:55:59 +0800

    fix map 书写格式

--- the_way_to_go/8.5.map_sort.go
+++ the_way_to_go/8.5.map_sort.go
@@ -7,10 +7,18 @@ import (
 
 var (
 	barVal = map[string]int{
-		"alpha": 34, "bravo": 56, "charlie": 23,
-		"delta": 87, "echo": 56, "foxtrot": 12,
-		"golf": 34, "hotel": 16, "indio": 87,
-		"juliet": 65, "kili": 43, "lima": 98,
+		"alpha":   34,
+		"bravo":   56,
+		"charlie": 23,
+		"delta":   87,
+		"echo":    56,
+		"foxtrot": 12,
+		"golf":    34,
+		"hotel":   16,
+		"indio":   87,
+		"juliet":  65,
+		"kili":    43,
+		"lima":    98,
 	}
 )
 
