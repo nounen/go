a89f7a6d19e18c4344d26b50431e03f8e739cd75
nounen <281580047@qq.com>
2018-06-12 21:21:05 +0800

    类型推导: 变量的类型由右值推导得出

--- z.practices/variables/variable_short.go
+++ z.practices/variables/variable_short.go
@@ -5,14 +5,15 @@ import (
 )
 
 // 短变量声明： 只能用在函数内， 函数外使用 var
-func main()  {
-    i := 1
+func main() {
+	// 类型推导: 变量的类型由右值推导得出
+	i := 1
 
-    fmt.Println(i)
+	fmt.Println(i)
 
-    // %v 输出为相应值的默认格式
-    fmt.Printf("%v \n", i)
+	// %v 输出为相应值的默认格式
+	fmt.Printf("%v \n", i)
 
-    // %T 格式化输出该值的类型
-    fmt.Printf("%T \n", i)
+	// %T 格式化输出该值的类型
+	fmt.Printf("%T \n", i)
 }
