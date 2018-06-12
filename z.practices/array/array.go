ca9f172c9b896ec8cbb3c3827c8da7422d476502
nounen <281580047@qq.com>
2018-06-12 23:11:51 +0800

    数组

---
+++ z.practices/array/array.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// 类型 [n]T 表示拥有 n 个 T 类型的值的数组
+	var str [2]string
+
+	str[0] = "Hello"
+	str[1] = "World"
+
+	// [Hello World]
+	fmt.Println(str)
+
+	// Hello World
+	fmt.Println(str[0], str[1])
+
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(primes)
+}
