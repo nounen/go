91bbc337c90456b5ac2f987f132c254f231d8ef0
nounen <281580047@qq.com>
2018-06-12 22:59:31 +0800

    结构体指针 的第二种声明方式

--- z.practices/structs/struct_pointer.go
+++ z.practices/structs/struct_pointer.go
@@ -11,6 +11,7 @@ type Vertex struct {
 func main() {
 	v := Vertex{1, 2}
 
+	// 1: 结构体指针
 	p := &v
 
 	// TODO: 这里明明只是取地址, 怎么就能直接赋值了, 应该是 *p.X = 99 来赋值才对
@@ -18,4 +19,19 @@ func main() {
 	p.X = 99
 
 	fmt.Println(v)
+	// Type: *main.Vertex Value: &{99 2}
+	fmt.Printf("Type: %T Value: %v\n", p, p)
+
+	// 2: 结构体指针
+	VPointer1 := &Vertex{}
+	VPointer2 := &Vertex{}
+
+	VPointer1.X = 1
+	VPointer1.Y = 1
+
+	// Type: *main.Vertex Value: &{1 1}
+	fmt.Printf("Type: %T Value: %v\n", VPointer1, VPointer1)
+
+	// Type: *main.Vertex Value: &{0 0}
+	fmt.Printf("Type: %T Value: %v\n", VPointer2, VPointer2)
 }
