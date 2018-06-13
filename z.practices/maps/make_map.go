a9ed39695c0126b59a081c942e2291b6b25f1b3f
nounen <281580047@qq.com>
2018-06-13 21:44:48 +0800

    map struct: 映射结构体

--- z.practices/maps/make_map.go
+++ z.practices/maps/make_map.go
@@ -10,6 +10,15 @@ type Vertex struct {
 
 var m map[string]Vertex
 
+var mm = map[string]Vertex{
+	"Bell Labs": Vertex{
+		40.68433, -74.39967,
+	},
+	"Google": Vertex{
+		37.42202, -122.08408,
+	},
+}
+
 func main() {
 	// 映射将键映射到值
 	// 映射的零值为 nil 。nil 映射既没有键，也不能添加键
@@ -22,4 +31,6 @@ func main() {
 
 	// {40.68433 -74.39967}
 	fmt.Println(m["Bell Labs"])
+
+	fmt.Println(mm)
 }
