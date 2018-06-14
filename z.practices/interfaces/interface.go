b92e65dc79a5d548ba0d46781a5ba3bfce31e62d
nounen <281580047@qq.com>
2018-06-14 23:51:50 +0800

    interface 的实现纠正错误

--- z.practices/interfaces/interface.go
+++ z.practices/interfaces/interface.go
@@ -19,11 +19,16 @@ type Vertex struct {
 }
 
 // Vertex 结构体实现接口的方法
-func (v *Vertex) Abs() float64 {
+// 注意: 实现结构体就不能以 指针作为接收者, 如果这里写成 `func (v V*ertex) Abs()` 那么就是错的!!!
+func (v Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
 func main() {
 	v := Vertex{3, 4}
 	fmt.Printf("%T %v \n", v, v)
+
+	// 使用 接口
+	var getInterFromStruct Abser = Vertex{3, 4}
+	fmt.Printf("%T %v \n", getInterFromStruct, getInterFromStruct)
 }
