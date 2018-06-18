06a2eb5b4f0ca9cb43533a1155adf5bf551c824d
nounen <281580047@qq.com>
2018-06-18 17:54:10 +0800

    带缓冲的信道

---
+++ 1.tour/5.concurrency/buffered-channels.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	ch := make(chan int, 2)
+
+	ch <- 1
+	ch <- 2
+
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+
+	// atal error: all goroutines are asleep - deadlock!
+	fmt.Println(<-ch)
+}
