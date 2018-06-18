a0f9e741efe72698b07a14fb0e02d7dab0d3ac1a
nounen <281580047@qq.com>
2018-06-18 17:46:54 +0800

    信道: chan 类型

---
+++ 1.tour/5.concurrency/channels.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func sum(s []int, c chan int) {
+	sum := 0
+
+	for _, v := range s {
+		sum += v
+	}
+
+	c <- sum // 将和送入 c
+}
+
+func main() {
+	s := []int{7, 2, 8, -9, 4, 0}
+
+	c := make(chan int)
+
+	fmt.Println(s[:len(s)/2])
+	go sum(s[:len(s)/2], c)
+
+	fmt.Println(s[len(s)/2:])
+	go sum(s[len(s)/2:], c)
+
+	x, y := <-c, <-c // 从 c 中接收
+
+	fmt.Println(x, y, x+y)
+}
