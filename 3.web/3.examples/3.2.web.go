69b920fa33d4949bc11dc4952c6ae161cf8f1556
nounen <281580047@qq.com>
2018-07-19 22:13:09 +0800

    3.2 web 生命周期理解

--- 3.web/3.examples/3.2.web.go
+++ 3.web/3.examples/3.2.web.go
@@ -7,6 +7,9 @@ import (
 	"strings"
 )
 
+// 全局变量, 不同浏览器打开也会不断累加 -- 生命周期与 脚本语言的不同之处
+var globalCount int = 0
+
 // http包建立Web服务器
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
@@ -20,6 +23,14 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("key:", k)
 		fmt.Println("val:", strings.Join(v, ""))
 	}
+
+	// 全局
+	globalCount++
+	fmt.Println(globalCount)
+
+	localCount := 1
+	localCount++
+	fmt.Println(localCount)
 
 	fmt.Fprintf(w, "Hello astaxie!")
 }
