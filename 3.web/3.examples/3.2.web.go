117348165cdaea719326261de5cd1c00d7302511
nounen <281580047@qq.com>
2018-07-19 22:27:00 +0800

    web

--- 3.web/3.examples/3.2.web.go
+++ 3.web/3.examples/3.2.web.go
@@ -15,6 +15,7 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 
 	fmt.Println(r.Form)
+	// TODO: 为什么默认会有 /favicon.ico 这个请求?
 	fmt.Println("path", r.URL.Path)
 	fmt.Println("scheme", r.URL.Scheme)
 	fmt.Println(r.Form["url_long"])
@@ -32,11 +33,16 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	localCount++
 	fmt.Println(localCount)
 
+	//这个写入到 w 的是输出到客户端的
 	fmt.Fprintf(w, "Hello astaxie!")
+	fmt.Fprintf(w, "Hello astaxie!")
+	fmt.Println()
 }
 
 func main() {
 	http.HandleFunc("/", sayhelloName)
+
+	fmt.Println("localhost:9090 web start!")
 
 	err := http.ListenAndServe(":9090", nil)
 
