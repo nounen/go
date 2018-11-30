9b62b2fd89774a8a7c96eb966f81408686c745eb
nounen <1394139539@qq.com>
2018-11-30 09:06:06 +0800

    Programs start running in package `main`

--- 1.tour/1.basics/1.packages.go
+++ 1.tour/1.basics/1.packages.go
@@ -1,13 +1,13 @@
-// 程序运行的入口是包 main 
+// 程序运行的入口是包 main
 package main
 
-// 这个程序使用并导入了包 "fmt" 和 "math/rand" 
+// 这个程序使用并导入了包 "fmt" 和 "math/rand"
 import (
 	"fmt"
 	"math/rand"
 )
 
-func main()  {
+func main() {
 	// 这个程序的运行环境是确定性的，因此 rand.Intn 每次都会返回相同的数字
 	fmt.Println("My favorite number is", rand.Intn(10))
 }
