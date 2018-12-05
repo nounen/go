409a5ec2eb8570d2e264c870e5bb4d48dd1a3bcf
nounen <1394139539@qq.com>
2018-12-05 14:16:30 +0800

    7.2 slice len cap make切片 new切片

---
+++ 2.Go编程基础(the_way_to_go)代码示例/第7章_数组与切片/7.2.make_slice.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	var slice1 []int = make([]int, 3)
+	fmt.Println(slice1)
+
+	// load the array/slice:
+	for i := 0; i < len(slice1); i++ {
+		slice1[i] = 5 * i
+	}
+
+	// print the slice:
+	for i := 0; i < len(slice1); i++ {
+		fmt.Printf("Slice at %d is %d\n", i, slice1[i])
+	}
+
+	fmt.Printf("\nThe length of slice1 is %d\n", len(slice1))
+	fmt.Printf("The capacity of slice1 is %d\n", cap(slice1))
+
+	// 对比 new，返回是指针
+	var slice2 *[]int = new([]int)
+	fmt.Println(slice2) // &[]
+}
