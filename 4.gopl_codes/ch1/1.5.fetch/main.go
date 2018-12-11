5aef358d9e444ad1ad94a6a89bfc15ca4d6a538c
nounen <1394139539@qq.com>
2018-12-11 17:48:10 +0800

    1.5 go channel 并发抓取 url

--- 4.gopl_codes/ch1/1.5.fetch/main.go
+++ 4.gopl_codes/ch1/1.5.fetch/main.go
@@ -14,6 +14,7 @@ import (
 	"os"
 )
 
+// go run 1.5.fetch/main.go http://www.baidu.com http://www.163.com
 func main() {
 	for _, url := range os.Args[1:] {
 		resp, err := http.Get(url)
@@ -21,13 +22,16 @@ func main() {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
+
 		b, err := ioutil.ReadAll(resp.Body)
 		resp.Body.Close()
+
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", b)
+
+		fmt.Printf("%s 的内容长度为 %d \n", url, len(string(b)))
 	}
 }
 
--- 4.gopl_codes/ch1/1.5.fetchall/main.go
+++ 4.gopl_codes/ch1/1.5.fetchall/main.go
@@ -16,15 +16,21 @@ import (
 	"time"
 )
 
+// go run 1.5.fetchall/main.go http://www.baidu.com http://www.163.com
 func main() {
 	start := time.Now()
 	ch := make(chan string)
+
 	for _, url := range os.Args[1:] {
-		go fetch(url, ch) // start a goroutine
+		// 开始 goroutine. start a goroutine
+		go fetch(url, ch)
 	}
+
 	for range os.Args[1:] {
-		fmt.Println(<-ch) // receive from channel ch
+		// 接受 channel. receive from channel ch
+		fmt.Println(<-ch)
 	}
+
 	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
 }
 
@@ -32,7 +38,8 @@ func fetch(url string, ch chan<- string) {
 	start := time.Now()
 	resp, err := http.Get(url)
 	if err != nil {
-		ch <- fmt.Sprint(err) // send to channel ch
+		// 发送 channel. send to channel ch
+		ch <- fmt.Sprint(err)
 		return
 	}
 
@@ -42,6 +49,7 @@ func fetch(url string, ch chan<- string) {
 		ch <- fmt.Sprintf("while reading %s: %v", url, err)
 		return
 	}
+
 	secs := time.Since(start).Seconds()
 	ch <- fmt.Sprintf("%.2fs  %7d  %s", secs, nbytes, url)
 }
