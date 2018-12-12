9f7203075eda4fcc87723fe6b655e922941ed2ec
nounen <1394139539@qq.com>
2018-12-12 14:33:25 +0800

    6.1. 方法声明

--- 4.gopl_codes/ch6/6.1.geometry/geometry.go
+++ 4.gopl_codes/ch6/6.1.geometry/geometry.go
@@ -17,6 +17,7 @@ func Distance(p, q Point) float64 {
 }
 
 // same thing, but as a method of the Point type
+// 下面的代码里那个附加的参数p，叫做方法的接收器(receiver)
 func (p Point) Distance(q Point) float64 {
 	return math.Hypot(q.X-p.X, q.Y-p.Y)
 }
